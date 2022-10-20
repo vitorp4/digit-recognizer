aa58cb25d696ea2d003154da379dc0ce44d67bd3
vitorp4 <jvsg.datac@gmail.com>
2022-10-20 11:06:44 -0300

    using tensorflow-cpu

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from fastapi.responses import HTMLResponse
 import numpy as np
 from fastapi import Body
 from pydantic import BaseModel
-import requests
+from keras.models import load_model
 from typing import List
 
 class NeuralNetworkPrediction(BaseModel):
@@ -30,27 +30,20 @@ async def classifier(matrix = Body()):
   digit_mlp = digit.reshape((1, 28*28)).astype('float32') / 255
   digit_convnet = digit.reshape((1, 28, 28, 1)).astype('float32') / 255
 
-  requests = [
-    ('mlp', 'http://localhost:8501/v1/models/mlp:predict', digit_mlp),
-    ('convnet', 'http://localhost:8501/v1/models/convnet:predict', digit_convnet)
-  ]
+  mlp_model = load_model('neural_networks/mlp_model')
+  convnet_model = load_model('neural_networks/convnet_model')
 
-  response = []
+  probabilities_mlp = mlp_model.predict(digit_mlp)
+  probabilities_convnet = convnet_model.predict(digit_convnet)
 
-  for model_name, url, body in requests:
-    probabilities = make_prediction(url, body)
-    response.append(NeuralNetworkPrediction(
-      model = model_name,
-      probabilities = probabilities,
-      digit = np.array(probabilities).argmax()
-    ))
+  response = [{
+    'model': 'mlp',
+    'probabilities': probabilities_mlp[0].tolist(),
+    'digit': probabilities_mlp[0].argmax()
+  },{
+    'model': 'convnet',
+    'probabilities': probabilities_convnet[0].tolist(),
+    'digit': probabilities_convnet[0].argmax()
+  }]
 
   return response
-
-def make_prediction(url, instances):
-   data = {"signature_name": "serving_default", "instances": instances.tolist()}
-   response = requests.post(url, json=data)
-   predictions = response.json()['predictions']
-   return predictions[0]
-
-  # Jinja2, fastapi, uvicorn[standard], tensorflow, requests
